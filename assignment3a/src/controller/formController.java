809f7cc8f38ec323142cf65048790dc7f51a908f
Mmohler1 <71154838+Mmohler1@users.noreply.github.com>
2020-10-16 21:05:11 -0700

    Add files via upload

---
+++ assignment3a/src/controller/formController.java
@@ -0,0 +1,36 @@
+//Michael Mohler 
+
+package controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import beans.User;
+
+@ManagedBean
+@ViewScoped
+public class formController 
+{
+	//Submit by initializing the user in the method 
+	public String onSubmit()
+	{
+		//Get the User Managed bean
+		FacesContext context = FacesContext.getCurrentInstance();
+		User user = context.getApplication().evaluateExpressionGet(context, "#{user}", User.class);
+		
+		//Forward to Test Response View along with the user Managed Bean
+		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
+		return "TestResponse.xhtml";
+	}
+	
+	
+	//Submit by the user in the test form using the methods parameters.
+	public String onSubmit(User user)
+	{
+		//Forward to test response view with the user managed bean.
+		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
+		return "TestResponse.xhtml";
+	}
+	
+}
